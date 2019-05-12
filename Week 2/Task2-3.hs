f70d46e016fe4cc2cc7dd2d16077bcd80582a441
Pavel <imran.pavel@gmail.com>
2019-05-12 16:49:47 +0300

    Refactoring

--- Week 2/Task2-3.hs
+++ Week 2/Task2-3.hs
@@ -1,76 +1,49 @@
-{-|
+{-
 
 Make a function that, given a list xs, a number n, and a function f, constructs a list ys such that: 
+
 ys has n elements,
 ys is constructed of elements of xs, and
 f evaluates to true on ys.
-If xs = "noway", k = 2, and f always returns true (e.g. f _ = True), then we get the same list as in the example for Task 2.2.
+If xs = "noway", n = 2, and f always returns true (e.g. f _ = True), then we get the same list as in the example for Task 2.2.
 
--}
-
-
-
-{-|
-    Call like this:       getYs "noway" 2 testFunction
-    or may be like this   getYs [1,2,3] 2 testFunction
-
-    You can give some other list and function as well.
--}
+Edit:
+(1) It is acceptable to apply f on the whole list (thereby either rejecting the answer or accepting it,
+    maybe return [] in this case) or on each individual element (thereby rejecting or accepting each element of the list).
+(2) You may either generate a list of length n of elements of xs, or a list of lists of length n.
 
 
-{-|
-
-How it works:
-Traverse the given list. At each item either create a list taking the item and create another ignoring the item. Keep count when moving to the
-next item.
-Once n items taken, treat "ys" as a new separate item (making it a list like this [ys]).
-If "givenList" is exhausted but "ys" still doesn't have n items, then check cases in Comment 1 and 2 and continue.
+To run try like any of these:
+-- taskFunction [1,2,3] 2 (\ _ -> True)
+-- taskFunction [1,2] 3 (\ _ -> True)
+-- taskFunction "abc" 2 (\ (x:_) -> x == 'b')
 
 
 Comment 1:
-Meaning a complete pass of traversing "givenList" has been completed without any addition of element in "ys".
-This happens when solutionFunction goes to "ignoreX" only when making choice.
-So after one pass if there is no update in "ys" then stop.
-
+Meaning, one pass of traversing givenList is complete. But no elements were taken, only ignored. Continuing will end up in infinite loop.
 
 Comment 2:
-One pass of traversing the "givenList" is complete, "ys" has some new elements but still does not have n elements.
-So, start another pass from the beginning of the "givenList" but this time update "initalYs" with "ys".
-
+Meaning, something was taken while traversing. [] indicates we've traversed the given list once. But there is still more to take,
+since currentN is not. So start again with the given list and change initalN.
 -}
-
-
 
 import qualified Data.List as DL
 
+getAllPossibleListsOfLengthN :: (Eq a) => [a] -> Int -> Int -> [a] -> [a] -> [[a]]
+
+getAllPossibleListsOfLengthN [] currentN initialN generatedList givenList
+    | currentN == initialN = []   -- Comment 1. Check Above.
+    | otherwise = getAllPossibleListsOfLengthN givenList currentN currentN generatedList givenList  -- Comment 2. Check Above.
+
+getAllPossibleListsOfLengthN (x:xs) 0 _ generatedList _ = [generatedList]
+getAllPossibleListsOfLengthN (x:xs) currentN initialN generatedList givenList =
+    DL.nub $ takeX ++ ignoreX
+    where
+        takeX = getAllPossibleListsOfLengthN xs (currentN-1) initialN (x:generatedList) givenList
+        ignoreX = getAllPossibleListsOfLengthN xs currentN initialN generatedList givenList
 
 
-testFunction _ = True
-
-
-solutionFunction :: (Eq a) => [a] -> Int -> [a] -> [a] -> ([[a]] -> Bool) -> Int -> [a] -> [[a]]
-solutionFunction _ 0 ys _ _ n _
-                            | length ys == n = [ys]
-                            | otherwise = []
-
-
-solutionFunction [] remainingN ys initialYs f n givenList
-                                                        | ys == initialYs = []              -- Comment 1
-                                                        | otherwise = solutionFunction givenList remainingN ys ys f n givenList             -- Comment 2
-
-
-solutionFunction (x:xs) remainingN ys initialYs f n givenList = DL.nub $ (checkWithF f takeX) ++ (checkWithF f ignoreX)
-                                                        where
-                                                            takeX = solutionFunction xs (remainingN-1) (x:ys) initialYs f n givenList
-                                                            ignoreX = solutionFunction xs remainingN ys initialYs f n givenList
-
-
-
-checkWithF f currentList
-                        | f currentList = currentList
-                        | otherwise = []
-
-
-
-getYs xs n f = solutionFunction xs n [] [] f n xs
-
+taskFunction :: (Eq a) => [a] -> Int -> ([a] -> Bool) -> [[a]]
+taskFunction xs n f = filter f candidateList
+    where
+        candidateList = getAllPossibleListsOfLengthN xs n n [] xs
